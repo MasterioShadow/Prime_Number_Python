2268a324aacdbbd24ffc7992000a3be25ec060c5
MasterioShadow <109391810+MasterioShadow@users.noreply.github.com>
2023-09-17 07:06:41 +0200

    Optimized  logic & encapsulated it in a function

--- app2 prime number checker.py
+++ app2 prime number checker.py
@@ -1,35 +1,31 @@
 import math
 
-# Prompt the user to input the number they want to check for primality.
+# Function to check if a number is prime
+def is_prime(num):
+    # Handle boundary cases:
+    if num <= 1:            # 1, 0 and -ve aren't primes
+        return False
+    elif num <= 3:          #anything in between 1 and 4 is prime for sure (2,3)
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    
+    # Check odd divisors up to the square root of num (rounded up).
+    for i in range(3, math.isqrt(num) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+# Input validation loop
 while True:
     try:
-        num = int(input(
-            'Please provide the number you\'d like to determine whether it\'s prime or not: '))
-    except ValueError:
-        print("\nInvalid input data type! You must enter an integer.\n")
-    else:
+        num = int(input('\n Please provide an integer to check for primality: '))
         break
-# Handle boundary cases:
-# 1, 0 and -ve aren't primes, and 2 is prime, so check these conditions first.
-if num <= 1:
-    print("\n", num, 'is not a prime number \n')
-elif num == 2:
-    print("\n", num, 'is a prime number \n')
-# If num is even, it's not prime (except for 2).
-elif num % 2 == 0:
-    print("\n", num, 'is not a prime number')
-else:
-    # Initialize a variable to track primality.
-    is_prime = True
-    # Check odd divisors up to the square root of num.
-    for i in range(3, int(math.sqrt(num)) + 1, 2):
-        if num % i == 0:
-            # If num is divisible by i, it's not prime.
-            is_prime = False
-            break
+    except ValueError:
+        print("Invalid input data type! You must enter an integer.")
 
-    # Determine and print the result.
-    if is_prime:
-        print("\n", num, 'is a prime number \n')
-    else:
-        print("\n", num, 'is not a prime number \n')
+# Check if the provided number is prime
+if is_prime(num):
+    print("\n", num, 'is a prime number \n')
+else:
+    print("\n", num, 'is not a prime number \n')
